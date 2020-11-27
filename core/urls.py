7f13aa082e993a446ccfbcbbe77beaa04b584017
warrinot <warrinot@gmail.com>
2020-11-27 17:40:54 +0300

    initial

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RecipeViewSet
+
+
+router = DefaultRouter()
+router.register('recipes', RecipeViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))]
